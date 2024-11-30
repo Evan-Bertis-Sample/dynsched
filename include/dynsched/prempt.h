0d0e2f302784031d7f932f060b5c0c86401a65e1
evan-bertis-sample <esample21@gmail.com>
2024-11-30 17:57:31 -0500

    Finally building, seperate function implementations to sources to prevent linker errors accross different compilation units. Moved esp32 implementation stuff over to own folder

--- include/dynsched/prempt.h
+++ include/dynsched/prempt.h
@@ -34,18 +34,14 @@ typedef struct dynsched_prempt_interface {
 
 } dynsched_prempt_interface_t;
 
-dynsched_prempt_interface_t *dynsched_prempt_create(dynsched_mem_manager_t *mem_manager, dynsched_prempt_interface_t interface, void *config) {
-    dynsched_prempt_interface_t *instance = interface.create(mem_manager, config);
-    return instance;
-}
-
-void dynsched_prempt_destroy(dynsched_prempt_interface_t *prempt) { prempt->destroy(prempt); }
-void dynsched_prempt_init(dynsched_prempt_interface_t *prempt) { prempt->init(prempt->platform_ctx); }
-void dynsched_prempt_start(dynsched_prempt_interface_t *prempt) { prempt->start(prempt->platform_ctx); }
-void dynsched_prempt_stop(dynsched_prempt_interface_t *prempt) { prempt->stop(prempt->platform_ctx); }
-void dynsched_prempt_prempt(dynsched_prempt_interface_t *prempt, dynsched_prempt_args_t *args) { prempt->prempt(prempt->platform_ctx, args); }
-void dynsched_prempt_lock(dynsched_prempt_interface_t *prempt) { prempt->lock(prempt->platform_ctx); }
-void dynsched_prempt_unlock(dynsched_prempt_interface_t *prempt) { prempt->unlock(prempt->platform_ctx); }
+dynsched_prempt_interface_t *dynsched_prempt_create(dynsched_mem_manager_t *mem_manager, dynsched_prempt_interface_t interface, void *config);
+void dynsched_prempt_destroy(dynsched_prempt_interface_t *prempt);
+void dynsched_prempt_init(dynsched_prempt_interface_t *prempt);
+void dynsched_prempt_start(dynsched_prempt_interface_t *prempt);
+void dynsched_prempt_stop(dynsched_prempt_interface_t *prempt);
+void dynsched_prempt_prempt(dynsched_prempt_interface_t *prempt, dynsched_prempt_args_t *args);
+void dynsched_prempt_lock(dynsched_prempt_interface_t *prempt);
+void dynsched_prempt_unlock(dynsched_prempt_interface_t *prempt);
 
 #ifdef __cplusplus
 }
